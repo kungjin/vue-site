45fa6d65bcf3ca5ef66c6b30e03bf8d7f44a0941
kungjin <vudchsdl123@gmail.com>
2025-08-25 13:24:55 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import D3_show from './components/D3_show.vue';
+
+
+</script>
+
+<template>
+
+<D3_show />
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/MyHellow.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+
+
+const msg = ref("msg")
+</script>
+
+
+<template>
+
+    <p>test</p>
+    <div>{{ msg }}</div>
+
+</template>
+
+
+<style>
+p{
+    background-color: aqua;
+}
+div{
+    border: 3px solid salmon;
+}
+
+</style>
